74c1ddeafe8af9c9a12b43ac8db5e405873028dd
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2023-02-28 04:52:02 +0900

    feat: installed and setup pundit

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::API
+  include Pundit::Authorization
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  rescue_from Pundit::NotAuthorizedError,   with: :user_not_authorized
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
@@ -8,5 +12,17 @@ class ApplicationController < ActionController::API
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
+  private
+
+  def user_not_authorized(exception)
+    render json: {
+      error: "Unauthorized access or action"
+    }, status: :unauthorized
+  end
+
+  def not_found(exception)
+    render json: { error: exception.message }, status: :not_found
   end
 end
