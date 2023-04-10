666c6a534a8f057354534633d87fe8224c5d146f
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2023-04-10 12:29:28 +0900

    feat: added messaging for non-admin users when checking invitations

--- front/myapp-front/src/components/elementsTab/ElementsTab.tsx
+++ front/myapp-front/src/components/elementsTab/ElementsTab.tsx
@@ -1,6 +1,6 @@
 import { TabContext, TabList, TabPanel } from "@mui/lab";
-import { Box, Tab } from "@mui/material";
-import { useState } from "react";
+import { Box, Tab, Typography } from "@mui/material";
+import { useContext, useState } from "react";
 import { CompoundTabProps } from "./ElementsTab.types";
 import { v4 as uuidv4 } from "uuid";
 import TaskCard from "../card/TaskCard";
@@ -13,9 +13,13 @@ import {
   InvitationDetails,
   TagDetails,
 } from "../../types/interfaces";
+import { UserContext } from "../../context/user/UserContext";
+import { GroupContext } from "../../context/group/GroupContext";
 
 function ElementsTab({ tabHeaders }: CompoundTabProps) {
   const [tabValue, setTabValue] = useState("1");
+  const { state: userState, dispatch: userDispatch } = useContext(UserContext);
+  const { state: groupState } = useContext(GroupContext);
 
   function handleTabChange(event: React.SyntheticEvent, newValue: string) {
     setTabValue(newValue);
@@ -54,12 +58,26 @@ function ElementsTab({ tabHeaders }: CompoundTabProps) {
                     />
                   );
                 case "invitation":
-                  return (
-                    <InvitationCard
-                      key={uuidv4()}
-                      element={el as InvitationDetails}
-                    />
-                  );
+                  if (
+                    userState.userObject.user.id === groupState.group.admin_id
+                  ) {
+                    return (
+                      <InvitationCard
+                        key={uuidv4()}
+                        element={el as InvitationDetails}
+                      />
+                    );
+                  } else {
+                    // If user is not admin of group they won't be able to see anything since it is already
+                    // filtered in the server side. But we might as well show some kind of message to the user
+                    return (
+                      <Box>
+                        <Typography>
+                          You have no authorization to see this content
+                        </Typography>
+                      </Box>
+                    );
+                  }
                 case "tag":
                   return <TagCard key={uuidv4()} element={el as TagDetails} />;
                 case "user":
@@ -77,19 +95,6 @@ function ElementsTab({ tabHeaders }: CompoundTabProps) {
         ))}
       </>
     </TabContext>
-
-    // <TabContext value={"3"}>
-    //   <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-    //     <TabList aria-label="lab API tabs example">
-    //       <Tab label="Item One" value="1" />
-    //       <Tab label="Item Two" value="2" />
-    //       <Tab label="Item Three" value="3" />
-    //     </TabList>
-    //   </Box>
-    //   <TabPanel value="1">Item One</TabPanel>
-    //   <TabPanel value="2">Item Two</TabPanel>
-    //   <TabPanel value="3">Item Three</TabPanel>
-    // </TabContext>
   );
 }
 
