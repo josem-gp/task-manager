dab003c5f1ebe2bbce08d3e8f1a46641cc68eba2
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2023-04-25 06:13:30 +0900

    feat: added logic to show popup in signup invitation

--- front/myapp-front/src/components/authForm/AuthForm.tsx
+++ front/myapp-front/src/components/authForm/AuthForm.tsx
@@ -1,11 +1,14 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Grid } from "@mui/material";
 import AuthFormLogin from "./AuthFormLogin";
 import AuthFormSignup from "./AuthFormSignup";
 import { useLocation } from "react-router-dom";
+import { PopupContext } from "../../context/popup/PopupContext";
+import ActionAlerts from "../actionAlerts/ActionAlerts";
 
 function AuthForm() {
   const [isLogin, setIsLogin] = useState(true);
+  const { popup } = useContext(PopupContext);
   // When the user is invited, they will have their email in the form of a query in the URL
   // In that case we want to render the Signup page with the set email by default
   const location = useLocation();
@@ -34,6 +37,11 @@ function AuthForm() {
           backgroundRepeat: "no-repeat",
         }}
       >
+        {/* 
+            Only for signup via invitation. if not, popup doesnt render since in the route we are calling AuthForm directly
+            and it didn't have the popup logic (only App had it) 
+        */}
+        {email && popup.message && <ActionAlerts severity="error" />}
         {isLogin ? (
           <AuthFormLogin setIsLogin={setIsLogin} />
         ) : (
